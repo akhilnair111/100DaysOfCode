4b36e4b4c4e69077dbaf554120abbca0d29b7ed3
Akhil Nair <44229684+akhilnair111@users.noreply.github.com>
2020-03-29 00:18:09 +0530

    Day [4/100] of #100DaysOfCode

---
+++ Week1/Every Three Numbers.py
@@ -0,0 +1,10 @@
+"""Create a function called every_three_nums that has one parameter named start.
+
+The function should return a list of every third number between start and 100 (inclusive). For example, every_three_nums(91) should return the list [91, 94, 97, 100]. If start is greater than 100, the function should return an empty list."""
+
+#Write your function here
+def every_three_nums(start):
+  return list(range(start, 101, 3))
+
+#Uncomment the line below when your function is done
+print(every_three_nums(101))
---
+++ Week1/Middle Item.py
@@ -0,0 +1,15 @@
+""" Create a function called middle_element that has one parameter named lst.
+
+If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements. """
+
+
+#Write your function here
+def middle_element(lst):
+  if len(lst) % 2 == 0:
+    sum = lst[int(len(lst)/2)] + lst[int(len(lst)/2) - 1]
+    return sum / 2
+  else:
+    return lst[int(len(lst)/2)]
+
+#Uncomment the line below when your function is done
+print(middle_element([5, 2, -10, -4, 4, 5]))
