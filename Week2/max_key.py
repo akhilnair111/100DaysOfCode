887a72dd49cbc7b42d40a47ef913dfc2cbc888e2
Akhil Nair <44229684+akhilnair111@users.noreply.github.com>
2020-04-03 15:30:07 +0530

    Day [9/100] of #100DaysOfCode

---
+++ Week2/max_key.py
@@ -0,0 +1,17 @@
+"""  Write a function named max_key that takes a dictionary named my_dictionary as a parameter. The function should return the key associated with the largest value in the dictionary.  """
+
+# Write your max_key function here:
+def max_key(my_dictionary):
+  largest_key = float("-inf")
+  largest_value = float("-inf")
+  for key, value in my_dictionary.items():
+    if value > largest_value:
+      largest_value = value
+      largest_key = key
+  return largest_key
+
+# Uncomment these function calls to test your  function:
+print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
