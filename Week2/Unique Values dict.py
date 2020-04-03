2084c1236f04e6b1c22dad369a4581be69b1aa62
Akhil Nair <44229684+akhilnair111@users.noreply.github.com>
2020-04-03 16:07:15 +0530

    Day [9/100] of #100DaysOfCode

---
+++ Week2/Unique Values dict.py
@@ -0,0 +1,15 @@
+"""  Create a function named unique_values that takes a dictionary named my_dictionary as a parameter. The function should return the number of unique values in the dictionary.  """
+
+# Write your unique_values function here:
+def unique_values(my_dictionary):
+  seen_values = []
+  for value in my_dictionary.values():
+    if value not in seen_values:
+      seen_values.append(value)
+  return len(seen_values)
+
+# Uncomment these function calls to test your  function:
+print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1
