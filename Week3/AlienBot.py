38365edce63d5f3d51300d195cb3ed980d55b73d
Akhil Nair <44229684+akhilnair111@users.noreply.github.com>
2020-04-20 12:43:58 +0530

    Day [20/100] of #100DaysOfCode

--- Week3/AlienBot.py
+++ Week3/AlienBot.py
@@ -20,8 +20,8 @@ class AlienBot:
 
   def __init__(self):
     self.alienbabble = {'describe_planet_intent': r'.*\s*your planet.*',
-                        'answer_why_intent': r'',
-                        'cubed_intent': r''
+                        'answer_why_intent': r'why\sare.*',
+                        'cubed_intent': r'.*cube.*(\d+)'
                             }
 
   # Define .greet() below:
@@ -45,32 +45,44 @@ class AlienBot:
   def chat(self):
     reply = input(random.choice(self.random_questions)).lower()
     while not self.make_exit(reply):
-      reply = input(self.match_reply())
+      reply = input(self.match_reply(reply))
           
     
 
   # Define .match_reply() below:
   def match_reply(self, reply):
     for intent, regex_pattern in self.alienbabble.items():
-      found_match = re.match(regex_patter, reply)
+      found_match = re.match(regex_pattern, reply)
       if found_match and intent == "describe_planet_intent":
         return self.describe_planet_intent()
+      elif found_match and intent == "answer_why_intent":
+        return self.answer_why_intent()
+      elif found_match and intent == "cubed_intent":
+        return self.cubed_intent(found_match.groups()[0])
+      else:
+        return self.no_match_intent()
+        
       
   # Define .describe_planet_intent():
   def describe_planet_intent(self):
-    return "Inside .describe_planet_intent()"
+    responses =  ("My planet is a utopia of diverse organisms and species. ", "I am from Opidipus, the capital of the Wayward Galaxies. ")
+    return random.choice(responses)
 
   # Define .answer_why_intent():
   def answer_why_intent(self):
-    return "Inside .answer_why_intent()"
+    responses = ("I come in peace. ", "I am here to collect data on your planet and its inhabitants. ", "I heard the coffee is good. ")
+    return random.choice(responses)
        
   # Define .cubed_intent():
   def cubed_intent(self, number):
-    return "Inside .cubed_intent()"
+    number = int(number)
+    cubed_number = number * number * number
+    return f"The cube of {number} is {cubed_number}. Isn't that cool? "
 
   # Define .no_match_intent():
   def no_match_intent(self):
-    return "Inside .no_match_intent()"
+    responses = ("Please tell me more. ", "Tell me more! ", "Why do you say that? ", "I see. Can you elaborate? ", "Interesting. Can you tell me more? ", "I see. How do you think? ", "Why? ", "How do you think I feel when you say that? ")
+    return random.choice(responses)
 
 # Create an instance of AlienBot below:
 my_bot = AlienBot()
